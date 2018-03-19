f5b137d7d5698dea10fc14815fe3c633f8315f08
Unknown <ahmetkocyigit97@gmail.com>
2018-03-19 11:05:57 +0100

    laravel project

---
+++ Laravel/resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <h1>About</h1>
+        <h2>test</h2>
+    </head>
+    <body>
+
+        
+    </body>
+</html>
---
+++ Laravel/resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+
+    <title>Laravel</title>
+    <h1>hallo</h1>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
